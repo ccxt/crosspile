a8fccffc67d5e8ffaf3832d930febb2a9a07775e
xpl <rocket.mind@gmail.com>
2017-09-07 01:16:57 +0300

    Initial commit

---
+++ crosspile.js
@@ -0,0 +1,6 @@
+module.exports = function crosspile (js, { from = 'JavaScript', to } = {}) {
+
+//  TODO: implement
+
+    return js
+}
---
+++ test.js
@@ -0,0 +1,21 @@
+/*  ------------------------------------------------------------------------ */
+
+const crosspile = require ('./crosspile')
+const assert    = require ('assert')
+const fs        = require ('fs')
+
+/*  ------------------------------------------------------------------------ */
+
+describe ('crosspile.js', () => {
+
+    it ('works', () => {
+
+        const js = fs.readFileSync ('./test_files/example.js', 'utf8')
+
+        const py  = crosspile (js, { to: 'Python' })
+        const php = crosspile (js, { to: 'PHP' })
+
+        assert.equal (py,  fs.readFileSync ('./test_files/example.py',  'utf8'))
+        assert.equal (php, fs.readFileSync ('./test_files/example.php', 'utf8'))
+    })
+})
